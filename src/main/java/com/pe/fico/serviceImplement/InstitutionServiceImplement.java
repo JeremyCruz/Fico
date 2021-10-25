9943d4dd59e071c9fc53d74c4a1c02b7ae41226e
NonSense96 <81791469+NonSense96@users.noreply.github.com>
2021-10-24 19:08:16 -0500

    Update name

--- src/main/java/com/pe/fico/serviceImplement/InstitutionServiceImplement.java
+++ src/main/java/com/pe/fico/serviceImplement/InstitutionServiceImplement.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pe.fico.entities.Institution;
-import com.pe.fico.repositories.IInstitutionRepository;
+import com.pe.fico.repositorie.IInstitutionRepositorie;
 import com.pe.fico.serviceinterfaces.IInstitutionService;
 
 @Service
 public class InstitutionServiceImplement implements IInstitutionService {
 	@Autowired
-	private IInstitutionRepository iR;
+	private IInstitutionRepositorie iR;
 	@Override
 	public Integer insert(Institution ins) {
 		// TODO Auto-generated method stub
