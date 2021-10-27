8d5dd5f8b5e6cb32fd9a0352d4162a1baae89e05
NonSense96 <81791469+NonSense96@users.noreply.github.com>
2021-10-27 00:01:26 -0500

    Servicio interface de tipo de cuenta de ahorro

---
+++ src/main/java/com/pe/fico/service/ITypeSavingAccountService.java
@@ -0,0 +1,14 @@
+package com.pe.fico.service;
+
+import java.util.List;
+
+import com.pe.fico.entities.TypeSavingAccount;
+
+public interface ITypeSavingAccountService {
+	
+	public Integer insert(TypeSavingAccount TipoCuentaAhorro);
+
+	List<TypeSavingAccount> list();
+
+
+}
