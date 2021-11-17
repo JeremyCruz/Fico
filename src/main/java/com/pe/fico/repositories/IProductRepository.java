9c098181b9076ab2082a2b9946a2dcd5bde31cfd
JeremyCruz <49539417+JeremyCruz@users.noreply.github.com>
2021-11-17 00:27:27 -0500

    Add files via upload

--- src/main/java/com/pe/fico/repositories/IProductRepository.java
+++ src/main/java/com/pe/fico/repositories/IProductRepository.java
@@ -1,5 +1,7 @@
 package com.pe.fico.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,9 @@ import com.pe.fico.entities.Product;
 public interface IProductRepository extends JpaRepository<Product, Integer> {
 	@Query("select count(l.nameProduct) from Product l where l.nameProduct=:name")
 	public int buscarProducto(@Param("name") String nombre);
+	
+	@Query(value="SELECT i.name_institution, cp.name_category_product, count(p.id_institution) "
+			+"from Product p join institution i on i.id_institution = p.id_institution "
+			+"join category_product cp on cp.id_category_product = p.id_categoryproduct group by i.name_institution, cp.name_category_product",nativeQuery=true)
+	public List<String[]>prodXcatXinst();
 }
