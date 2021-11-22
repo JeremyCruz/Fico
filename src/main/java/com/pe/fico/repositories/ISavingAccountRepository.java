0d5730855122fb7f892bad28022c122cbe7bc021
NonSense96 <81791469+NonSense96@users.noreply.github.com>
2021-11-21 23:10:03 -0500

    Update 4

--- src/main/java/com/pe/fico/repositories/ISavingAccountRepository.java
+++ src/main/java/com/pe/fico/repositories/ISavingAccountRepository.java
@@ -14,6 +14,16 @@ public interface ISavingAccountRepository extends JpaRepository<SavingAccountPro
 	@Query("select count(sa.minOpeningSaving) from SavingAccountProduct sa where sa.idSavingAccount=:name")
 	public int buscarCuentaAhorro(@Param("name") String nombre);
 	
+	@Query("select sa from SavingAccountProduct sa where sa.product.nameProduct like %?1%")
+	List<SavingAccountProduct>fetchProductByName(String nombre);
+	
 	@Query("select c from SavingAccountProduct c where c.freeOperationSA >= :op")
 	List<SavingAccountProduct> findByOper(int op);
+	
+	@Query("select sa from SavingAccountProduct sa where sa.tsa.nameTypeSavingAccount like %?1%")
+	List<SavingAccountProduct>fetchProductByTipo(String nombreTipo);
+	
+	@Query("select sa from SavingAccountProduct sa where sa.product.ratingProduct >= 4 ")
+	List<SavingAccountProduct>fetchBestProducts();
+	
 }
